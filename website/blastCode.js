e4b124afcbad8607c05e18341b5809ab095cb6e6
hershel <daniel.h.silber.baker@gmail.com>
2017-10-06 11:36:00 -0400

    upload local project for optimization

---
+++ website/blastCode.js
@@ -0,0 +1,140 @@
+$('#content').blast({
+  // delimiter: 'word',
+   search: "(yes)" ,
+
+   customClass: "response"
+});
+
+$('#content').blast({
+  // delimiter: 'word',
+
+    search:"(That’s right)",
+
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(All right, That’s right)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(No) ",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Yeah)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:" (Yes, That’s right)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(All right)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Yes sir)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Yes sir, Yes)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Yes) ",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Speak)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:" (Yes, All right)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Speak)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Well)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Speak)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:" (Yes, Go ahead) ",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:" (Speak)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Come on)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Yes sir)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Oh yeah)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Oh yes)",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:" (Speak sir) ",
+ 	
+   customClass: "response"
+});
+
+ $('#content').blast({
+ 	search:"(Well)",
+ 	
+   customClass: "response"
+});
